d9e04094c66649140592039a706dedf310026723
harrison <hsouchereau@outlook.com>
2020-05-10 01:03:22 -0300

    added temporary test files

--- kikite.py
+++ kikite.py
@@ -1,8 +1,10 @@
 """ Main file for data processings """
 
 import kikite as kk
-import numpy
-test_filename = "music/test.mp3"
+
+
+test_filename = "/home/harrison/Music/Reeder - A Quiet Place.mp3"
+
 
 
 kk.read_file(test_filename)
