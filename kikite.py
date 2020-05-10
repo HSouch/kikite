ecd20842fe223dcbfc1b1c4cbaa11a4b3ac3f063
DESKTOP-3DLAU7V\Harrison <hsouchereau@outlook.com>
2020-05-10 03:12:37 -0300

    Added test notebook

--- kikite.py
+++ kikite.py
@@ -1,9 +1,9 @@
-""" Main file for data processings """
+""" Main file for data processing """
 
 import kikite as kk
 
+import numpy
+
 test_filename = "test_data/test.mp3"
-
-
 
 kk.read_file(test_filename)
