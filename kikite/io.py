4788e8beda5b4359d7d39dfdd0b586808075e421
DESKTOP-3DLAU7V\Harrison <hsouchereau@outlook.com>
2020-05-10 21:15:21 -0300

    Updated verbose mode

--- kikite/io.py
+++ kikite/io.py
@@ -5,10 +5,11 @@ import warnings
 import librosa
 
 
-def read_file(filename, verbose=False):
+def read_file(filename, verbose=False, sample_rate=22050):
     """
     Reads in a given input file and converts it to workable data.
     :param filename:
+    :param sample_rate: sample rate when reading in file. Default is 22050 Hz
     :return:
     """
     warnings.filterwarnings("ignore")  # We don't care if librosa has to default to audiostream (I think)
@@ -17,7 +18,7 @@ def read_file(filename, verbose=False):
 
     try:
         extension = filename.split(".")[len(filename.split(".")) - 1]
-        y, sr = librosa.load(filename)
+        y, sr = librosa.load(filename, sr=sample_rate)
     except FileNotFoundError:
         print("Error." + filename + " does not exist.")
         exit(1)
@@ -26,9 +27,6 @@ def read_file(filename, verbose=False):
 
     if verbose:
         print(filename + " successfully loaded. Time elapsed: " + str(time.time() - t_start)[:6] + " seconds.")
+        print("Length of input audio: " + str(len(y) / sample_rate)[:10] + " seconds.")
 
     return y, sr
-
-
-def new_test():
-    return None
