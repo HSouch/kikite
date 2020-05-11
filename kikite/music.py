e5aa9317eb7d0ba5cca9cd5be3701c006232f88a
DESKTOP-3DLAU7V\Harrison <hsouchereau@outlook.com>
2020-05-10 21:15:33 -0300

    Added method stubs

---
+++ kikite/music.py
@@ -0,0 +1,11 @@
+""" Contains all processing of music related stuff from input frequencies """
+
+
+def freq_to_note(frequency):
+    """ Converts a frequency into a given musical note """
+    return None
+
+
+def determine_key(notes):
+    """ Estimates the key from a given set of notes. """
+    return None
