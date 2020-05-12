933edcab74ece4804761b6b1b5c7dbe4c1626af8
DESKTOP-3DLAU7V\Harrison <hsouchereau@outlook.com>
2020-05-12 01:27:26 -0300

    Updated setup info

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup, find_packages
 
 
 __name__ = "kikite"
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 __url__ = "https://github.com/HSouch/kikite"
 __author__ = "Harrison Souchereau"
 __author_email__ = "harrison.souchereau@yale.edu"
+
+
+req_file = open('required_packages.txt')
+__install_requires__ = [x.strip("\n") for x in req_file.readlines()]
 
 
 setup(
@@ -14,7 +18,10 @@ setup(
     author=__author__,
     author_email=__author_email__,
     description="Chord determination from audio files.",
+    long_description_content_type="text/markdown",
     url=__url__,
     packages=find_packages(),
 
+    install_requires=__install_requires__,
+    include_package_data=True
     )
